837b10ccfd572f1efc2ae1adb9602f57b89ab3bd
aircongo <natelufuluabo@yahoo.ca>
2023-12-11 17:57:30 -0500

    implemented the ability to execute the right function for genre dispatcher function

--- controllers/genre.js
+++ controllers/genre.js
@@ -1,3 +1,29 @@
 export function genreDispatcher(method, path) {
     // Call the appropriate function based on method and path received
+    switch (method) {
+        case 'POST':
+            return createRessources();
+        case 'PUT':
+            return updateRessources();
+        case 'DELETE':
+            return deleteRessources();
+        default:
+            return getRessources();
+    }
+}
+
+function getRessources() {
+    return 'genre ressources retrieved';
+}
+
+function createRessources() {
+    return 'genre ressources created';
+}
+
+function updateRessources() {
+    return 'genre ressources updated';
+}
+
+function deleteRessources() {
+    return 'genre ressources deleted';
 }
